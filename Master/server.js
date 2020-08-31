4311528127ef16dd8073325ecca295403c219650
IPv5 <donationbuycraft@gmail>
2020-08-31 15:55:06 -0700

    Organized files

--- Master/server.js
+++ Master/server.js
@@ -2,17 +2,13 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(logger('dev'));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
 var MONGODB_URI =
@@ -22,9 +18,7 @@ mongoose.connect(MONGODB_URI, {
   useFindAndModify: false,
 });
 
-// routes
 app.use(require('./routes/api.js'));
-
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
