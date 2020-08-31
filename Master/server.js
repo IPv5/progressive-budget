b362198a3f4c309bee66ac1faac932f2e1b14c81
IPv5 <donationbuycraft@gmail>
2020-08-31 15:57:48 -0700

    updated server.js for heroku implementation

--- Master/server.js
+++ Master/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 var MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost/avnoto-budget-tracker';
+  process.env.MONGODB_URI || 'mongodb://localhost/agsimone-budget-tracker';
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
